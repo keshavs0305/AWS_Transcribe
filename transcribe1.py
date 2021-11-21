c823b05aa0ef130dede62be62517d3f5ad7111fd
keshav <keshavsarda503@gmail.com>
2021-11-21 14:41:24 +0530

    developed

--- transcribe1.py
+++ transcribe1.py
@@ -1,24 +1,32 @@
 import time
 import boto3
 import os
+import json
+import urllib.request as urllib2
 
-s3 = boto3.resource('s3')
-for file in os.listdir('input_audios'):
-    s3.meta.client.upload_file('input_audios/' + file, 'sample-conversation-file-bucket', file)
+#s3 = boto3.resource('s3')
+#for file in os.listdir('input_audios'):
+#    s3.meta.client.upload_file('input_audios/' + file, 'sample-conversation-file-bucket', file)
 
 s3_client = boto3.client('s3')
 transcribe_client = boto3.client('transcribe')
 
-response = s3_client.list_objects('sample-conversation-file-bucket')
+response = s3_client.list_objects(
+    Bucket='sample-conversation-file-bucket'
+)
 for content in response['Contents']:
-    transcribe_client.start_transcription_job(
-        TranscriptionJobName=content['Key'],
-        LanguageCode='en-US',
-        MediaFormat='wav',
-        Media={
-            'MediaFileUri': 's3' + 'sample-conversation-file-bucket' + content['Key']
-        },
-    )
+    print(content['Key'])
+    try:
+        transcribe_client.start_transcription_job(
+            TranscriptionJobName=content['Key'],
+            LanguageCode='en-US',
+            MediaFormat='wav',
+            Media={
+                'MediaFileUri': 's3://' + 'sample-conversation-file-bucket/' + content['Key']
+            },
+        )
+    except:
+        print('already exist')
 
     max_tries = 60
     while max_tries > 0:
@@ -31,4 +39,10 @@ for content in response['Contents']:
         else:
             time.sleep(10)
 
-    print(text_file_s3_uri)
+    response = urllib2.urlopen(text_file_s3_uri)
+    data_json = json.loads(response.read())
+
+    file_name = 'output_text_files/'+content['Key']
+    text_file = open(file_name[:-3]+'txt', "w")
+    text_file.write(data_json['results']['transcripts'][0]['transcript'])
+    text_file.close()
